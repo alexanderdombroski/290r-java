49d103fbc031446be9aa1789c6a74e9e96237834
alexanderdombroski <dom21013@byui.edu>
2025-01-24 09:56:40 -0700

    Fix: Player notified if cannot move a specific direction

--- src/edu/byui/apj/adventure/Player.java
+++ src/edu/byui/apj/adventure/Player.java
@@ -12,15 +12,19 @@ class Player {
         switch (direction) {
             case "N" -> {
                 if (location.north != null) location = location.north;
+                else System.out.println("Cannot move that way");
             }
             case "S" -> {
                 if (location.south != null) location = location.south;
+                else System.out.println("Cannot move that way");
             }
             case "E" -> {
                 if (location.east != null) location = location.east;
+                else System.out.println("Cannot move that way");
             }
             case "W" -> {
                 if (location.west != null) location = location.west;
+                else System.out.println("Cannot move that way");
             }
             default -> System.out.printf("Unknown Direction %s; Cannot Move%n", direction);
         }
